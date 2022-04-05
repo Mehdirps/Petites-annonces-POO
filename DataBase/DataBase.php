4bff1bfbe777488b61a3d7f972d2b688305c7402
Mehdi Raposo <mehdi.raposo77@gmail.com>
2022-04-05 16:25:13 +0200

    Création fichier DataBase

---
+++ DataBase/DataBase.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataBase;
+
+use PDO;
+use PDOException;
+
+class DataBase extends PDO
+{
+    private static $instance;
+
+    private const DBHOST = 'localhost';
+    private const DBUSER = 'root';
+    private const DBPASS = '';
+    private const DBNAME = 'petites_annonces';
+
+    private function __construct()
+    {
+        $_dsn = 'mysql:dbname' . self::DBNAME . ';host=' . self::DBHOST;
+
+        try {
+            parent::__construct($_dsn, self::DBUSER, self::DBPASS);
+            /**
+             * $this = PDO
+             */
+            $this->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public static function getInstance():self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+}
